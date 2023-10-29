d64d828c458e4f34ffbbd16737cf40be51634811
tataosmonalieva <emusya1010@mail.ru>
2023-10-29 21:16:10 +0600

    home

---
+++ script.js
@@ -0,0 +1,52 @@
+//let a = 'hello'
+//let b = ' ivan'
+//console.log(a + b)
+
+//let a = 'lorem ipsum dolor'
+//let b = a.substr(0, 17)
+//console.log(b)
+
+//let a = ' lorem ipsum dolor  sit  amet'
+//let b = ' sit '
+//let c = b +  a.substring(0,18) + a.slice(-4)
+//console.log(c)
+
+//let a = 'children'
+//let b = a.slice(0, 7).toUpperCase() + a.slice(7)
+//console.log(b)
+
+//let a = 'boss'
+//let b = a.slice(0, 1) + a.slice(2) + a.slice(1,2)
+//console.log(b)
+
+//let a = 'sunday'
+//let b = a.indexOf('d')
+//console.log(b)
+
+//let b = 'hello'
+//let newstr = b.replace(b, 'hekso');
+//console.log(newstr)
+
+//let a = 'user'
+//let b = 'new '
+//let c = a.slice(0,2)
+//console.log(b + c)
+
+//let a = 'monday'
+//let b = 'day'
+//let c = b.slice(0,3) + b.slice(0,3) + a.slice(0,3)
+//console.log(c)
+
+//let a = 'HELLO'
+//let b = a.slice(0,1).toLowerCase() + a.slice(1,2).toUpperCase() + a.slice(2,3).toLowerCase() + a.slice(3,4).toUpperCase() + a.slice(4,5).toLowerCase()
+//console.log(b)
+
+//let a = 5
+//let b = '400'
+//let c = b.slice(0,1) + a + b.slice(1,2)
+//console.log(c)
+
+//let a = 'monkey'
+//let b = 'banana'
+//let c = a.slice(0,1) + b.slice(1,4) + a.slice(3,4)
+//console.log(c)
